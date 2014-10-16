7220ec3daf7734ef2c008e1ac8721a3f5b7853d3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-10-16 14:43:51 -0400

    setting and testing controller explicitly

--- test/controller-spec.js
+++ test/controller-spec.js
@@ -7,10 +7,20 @@ angular.module('D', [])
 ngDescribe({
   name: 'test controller',
   modules: 'D',
-  inject: '$controller',
+  inject: ['$controller', '$rootScope'],
   tests: function (deps) {
     it('has $controller function', function () {
       la(check.fn(deps.$controller));
+    });
+
+    it('can create new controller', function () {
+      var scope = deps.$rootScope.$new();
+      la(check.object(scope), 'created new scope');
+      var ctrl = deps.$controller('dController', {
+        $scope: scope
+      });
+      la(check.object(ctrl), 'got back controller instance', ctrl);
+      la(scope.foo === 'foo', 'scope has been updated');
     });
   }
 });

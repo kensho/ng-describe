e6d69cf13e89509a6320910475a9d56782c6a2e8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-08-11 15:42:12 -0400

    added unit test with mocked  and controllerAs syntax

---
+++ test/mock-state-spec.js
@@ -0,0 +1,31 @@
+angular.module('Position', [])
+  .controller('PositionController', function ($state) {
+    this.init = function init() {
+      return $state.params.name;
+    };
+    this.name = this.init();
+});
+
+/* global ngDescribe, it */
+ngDescribe({
+  name: 'mocking state params',
+  modules: 'Position',
+  controller: 'PositionController',
+  element: '<div ng-controller="PositionController as position"></div>',
+  mock: {
+    Position: {
+      $state: {
+        params: {
+          name: 'mockName'
+        }
+      }
+    }
+  },
+  tests: function (deps) {
+    it('has params name from $state', function () {
+      var ctrl = deps.element.controller();
+      la(ctrl.name === 'mockName');
+    });
+  }
+});
+

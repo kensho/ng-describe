f025b66c4d35984d6f3654c9a89fb8a45c768426
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-08-31 16:58:44 -0400

    working on loading ngDescribe

--- misc/load-ng-describe-from-node-spec.js
+++ misc/load-ng-describe-from-node-spec.js
@@ -1,5 +1,7 @@
-var benv = require('benv');
-describe('calc module', function () {
+describe('load ngDescribe in synthetic browser', function () {
+
+  var benv = require('benv'), ngDescribeFn;
+
   beforeEach(function setupEnvironment(done) {
     benv.setup(function () {
       console.assert(typeof window === 'object', 'has window');
@@ -7,16 +9,37 @@ describe('calc module', function () {
       check = require('check-more-types');
 
       benv.expose({
-        angular: benv.require('../node_modules/angular/angular.js', 'angular'),
-        ngDescribe: benv.require('../dist/ng-describe.js', 'ngDescribe')
+        angular: benv.require('../node_modules/angular/angular.js', 'angular')
       });
       done();
     });
   });
 
+  beforeEach(function loadLa() {
+    require('lazy-ass');
+  });
+
+  beforeEach(function loadNgDescribe() {
+    check = require('check-more-types');
+    console.assert(typeof check.fn === 'function', 'has check.or');
+    require('../src/ng-describe');
+    ngDescribeFn = window.ngDescribe;
+  });
+
   it('dummy test', function () {});
 
+  it('dummy test 2', function () {});
+
+  it('has window', function () {
+    console.assert(typeof window === 'object');
+  })
+
+  it.skip('has ngDescribe', function () {
+    console.assert(typeof ngDescribeFn === 'function',
+      'has ngDescribe function');
+  });
+
   afterEach(function destroySyntheticBrowser() {
-    benv.teardown(false);
+    benv.teardown(true);
   });
 });

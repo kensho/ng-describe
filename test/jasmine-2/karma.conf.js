5cec1d8cf8bffe5cbb3892f8dc2583e2a2a659dd
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-10-17 09:56:21 -0400

    linting

--- test/jasmine-2/karma.conf.js
+++ test/jasmine-2/karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function(config) {
         '../dist/ng-describe.js',
         '../node_modules/lazy-ass-helpful/lazy-ass-helpful-browser.js',
         '../node_modules/lazy-ass-helpful/lazy-ass-helpful-bdd.js',
+        '../node_modules/obind/index.js',
         './*-spec.js'
     ],
 
---
+++ test/obind-spec.js
@@ -0,0 +1,71 @@
+angular.module('identity', [])
+  .value('I', function (x) { 
+    return x; 
+  });
+
+/* global ngDescribe, it */
+// test same value multiple times by chaining calls
+ngDescribe({
+  name: 'test identity',
+  module: 'identity',
+  inject: 'I',
+  tests: function (deps) {
+    it('returns passed value', function () {
+      la(deps.I('foo') === 'foo');
+      console.log('test 1');
+    });
+  }
+})({
+  name: 'test identity 2',
+  module: 'identity',
+  inject: 'I',
+  tests: function (deps) {
+    it('returns passed value', function () {
+      la(deps.I('bar') === 'bar');
+      console.log('test 2');
+    });
+  }
+})({
+  name: 'test identity 3',
+  module: 'identity',
+  inject: 'I',
+  tests: function (deps) {
+    it('returns passed value', function () {
+      la(deps.I(42) === 42);
+      console.log('test 3');
+    });
+  }
+});
+
+// remove duplicate boilerplate using 'obind'
+/* global obind */
+var describeIdentity = obind(ngDescribe, {
+  name: 'test identity 3',
+  module: 'identity',
+  inject: 'I'
+});
+// same tests
+describeIdentity({
+  tests: function (deps) {
+    it('returns passed value', function () {
+      la(deps.I('foo') === 'foo');
+      console.log('test 1');
+    });
+  }
+});
+describeIdentity({
+  tests: function (deps) {
+    it('returns passed value', function () {
+      la(deps.I('bar') === 'bar');
+      console.log('test 2');
+    });
+  }
+});
+describeIdentity({
+  tests: function (deps) {
+    it('returns passed value', function () {
+      la(deps.I(42) === 42);
+      console.log('test 3');
+    });
+  }
+});

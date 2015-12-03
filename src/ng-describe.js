2ed81d983d8ac39c789e10f46cd3a17381a5c385
Martin Ratinaud <martinratinaud@gmail.com>
2015-12-03 16:45:25 +0100

    feat(dependencies): add support for headers and statusText in http mock

--- src/ng-describe.js
+++ src/ng-describe.js
@@ -406,6 +406,14 @@
           if (isResponsePair(value)) {
             return dependencies.http.when(method, url).respond(value[0], value[1]);
           }
+
+          if (Array.isArray(value) && value.length >= 3) {
+            return dependencies.http.when(method, url).respond(function (method, url, data, headers) {
+              var compiledHeaders = value[2] ? angular.extend(headers, value[2]) : headers;
+
+              return [value[0], value[1], compiledHeaders, value[3] || ''];
+            });
+          }
           return dependencies.http.when(method, url).respond(200, value);
         });
       }
---
+++ test/http-with-headers-spec.js
@@ -0,0 +1,30 @@
+/* global ngDescribe, it */
+ngDescribe({
+  name: 'mocking http with headers',
+  only: true,
+  verbose: false,
+  inject: '$http',
+  http: {
+    post: {
+      '/my/url': [201, {message: 'ok'}, {Location: '/new/url'}, 'this is the new response']
+    }
+  },
+  tests: function (deps) {
+    it('should append right headers to result', function (done) {
+      deps.$http.post('/my/url', {})
+        .then(function (data) {
+          la(data.headers('Location') === '/new/url');
+        })
+        .then(done);
+      deps.http.flush();
+    });
+    it('should append right statusText to result', function (done) {
+      deps.$http.post('/my/url', {})
+        .then(function (data) {
+          la(data.statusText === 'this is the new response');
+        })
+        .then(done);
+      deps.http.flush();
+    });
+  }
+});

fd3a8f20ef1a15f01795db98d3997a75f5a02701
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-02-22 14:14:06 -0500

    chore(test): trying to test order of callbacks

--- test/after-each-order-spec.js
+++ test/after-each-order-spec.js
@@ -1,18 +1,35 @@
-/* global describe, it, afterEach */
-describe('after each callbacks', function () {
+/* global describe, it, afterEach, after, afterAll */
+var suite = describe;
+// Mocha vs Jasmine
+var afterTests = typeof afterAll === 'function' ? afterAll : after;
+
+suite('after each callbacks', function () {
   // just a dummy unit test to figure out how afterEach callbacks run
-  it('passes', function () {});
+  var labels = '';
+  it('passes', function () {
+    labels += 'test';
+  });
 
   afterEach(function one() {
     console.log('one');
+    labels += '1';
   });
 
   afterEach(function one() {
     console.log('two');
+    labels += '2';
   });
 
   afterEach(function one() {
     console.log('three');
+    labels += '3';
+  });
+
+  // does NOT run in Jasmine?
+  afterTests(function checkOrder() {
+    la(labels === 'test123', 'invalid order', labels);
+    console.log('labels', labels);
+    // throw new Error('labels ' + labels);
   });
   /*
     result
